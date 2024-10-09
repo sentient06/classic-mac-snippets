4958b101fdf7682d0f8842c5f3be708573b81c73
Giancarlo Mariot <sentient.06@gmail.com>
2024-10-09 21:56:48 +0100

    Converted line endings to Unix

--- C/sha1.h
+++ C/sha1.h
@@ -1,1 +1,15 @@
-#ifndef SHA1_H#define SHA1_H#ifdef __cplusplusextern "C" {#endiftypedef unsigned long uint32_t;void sha1(const unsigned char *message, uint32_t length, uint32_t *digest);#ifdef __cplusplus}#endif#endif
+#ifndef SHA1_H
+#define SHA1_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned long uint32_t;
+void sha1(const unsigned char *message, uint32_t length, uint32_t *digest);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
